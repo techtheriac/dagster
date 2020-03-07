549c995c0ccfa3b6e99d915ccab66da9783eb3b9
asingh16 <abhi@elementl.com>
2020-03-06 16:17:23 -0800

    clean up and add bay_bikes examples to dagster_examples repo
    Summary: This revision cleans up the dagster_examples repo and integrates the bay bikes pipelines and schedules.
    
    Test Plan: na
    
    Reviewers:
    
    Subscribers:

--- examples/dagster_examples/__init__.py
+++ examples/dagster_examples/__init__.py
@@ -1,8 +1,13 @@
-from dagster import ScheduleDefinition, file_relative_path, schedules
+from dagster import schedules
 
 
-@schedules
-def define_scheduler():
+def get_bay_bikes_schedules():
+    from dagster_examples.bay_bikes.schedules import daily_weather_ingest_schedule, monthly_trip_ingest_schedule
+    return [daily_weather_ingest_schedule, monthly_trip_ingest_schedule]
+
+
+def get_toys_schedules():
+    from dagster import ScheduleDefinition, file_relative_path
     return [
         ScheduleDefinition(
             name="many_events_every_min",
@@ -45,10 +50,15 @@ def define_scheduler():
     ]
 
 
-def define_demo_repo():
-    # Lazy import here to prevent deps issues
+@schedules
+def define_scheduler():
+    return [
+        *get_bay_bikes_schedules(),
+        *get_toys_schedules(),
+    ]
 
-    from dagster import RepositoryDefinition
+
+def get_toys_pipeline_info():
     from dagster_examples.toys.error_monster import error_monster
     from dagster_examples.toys.sleepy import sleepy_pipeline
     from dagster_examples.toys.log_spew import log_spew
@@ -59,34 +69,62 @@ def define_demo_repo():
         pandas_hello_world_pipeline,
         pandas_hello_world_pipeline_with_read_csv,
     )
+    from dagster_examples.toys.unreliable import unreliable_pipeline
+    return [
+        composition,
+        error_monster,
+        log_spew,
+        many_events,
+        pandas_hello_world_pipeline,
+        pandas_hello_world_pipeline_with_read_csv,
+        sleepy_pipeline,
+        stdout_spew_pipeline,
+        unreliable_pipeline,
+    ]
+
+
+def get_airline_demo_pipeline_info():
     from dagster_examples.airline_demo.pipelines import (
         airline_demo_ingest_pipeline,
         airline_demo_warehouse_pipeline,
     )
+    return [
+        airline_demo_ingest_pipeline,
+        airline_demo_warehouse_pipeline,
+    ]
+
+
+def get_event_pipeline_info():
     from dagster_examples.event_pipeline_demo.pipelines import event_ingest_pipeline
+    return [event_ingest_pipeline]
+
+
+def get_pyspark_pipeline_info():
     from dagster_examples.pyspark_pagerank.pyspark_pagerank_pipeline import pyspark_pagerank
-    from dagster_examples.toys.unreliable import unreliable_pipeline
+    return [pyspark_pagerank]
 
-    # from dagster_pandas.examples import papermill_pandas_hello_world_pipeline
+
+def get_jaffle_pipeline_info():
     from dagster_examples.jaffle_dbt.jaffle import jaffle_pipeline
+    return [jaffle_pipeline]
 
+
+def get_bay_bikes_pipeline_info():
+    from dagster_examples.bay_bikes.pipelines import generate_training_set_and_train_model
+    return [generate_training_set_and_train_model]
+
+
+def define_demo_repo():
+    # Lazy import here to prevent deps issues
+    from dagster import RepositoryDefinition
     return RepositoryDefinition(
-        name='hello_cereal_repository',
+        name='internal-dagit-repository',
         pipeline_defs=[
-            pandas_hello_world_pipeline_with_read_csv,
-            pandas_hello_world_pipeline,
-            sleepy_pipeline,
-            error_monster,
-            log_spew,
-            many_events,
-            composition,
-            airline_demo_ingest_pipeline,
-            airline_demo_warehouse_pipeline,
-            event_ingest_pipeline,
-            pyspark_pagerank,
-            # papermill_pandas_hello_world_pipeline,
-            jaffle_pipeline,
-            stdout_spew_pipeline,
-            unreliable_pipeline,
-        ],
+            *get_airline_demo_pipeline_info(),
+            *get_bay_bikes_pipeline_info(),
+            *get_event_pipeline_info(),
+            *get_jaffle_pipeline_info(),
+            *get_pyspark_pipeline_info(),
+            *get_toys_pipeline_info(),
+        ]
     )
